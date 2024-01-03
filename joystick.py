6242a5c3d5f08d9c681aa5f7f967d5af08acbc08
Sanjog34 <108666448+Sanjog34@users.noreply.github.com>
2024-01-03 23:27:04 +0545

    joystick.py

--- joystick.py
+++ joystick.py
@@ -4,7 +4,7 @@ val=0
 
 def is_com4_connected():
     try:
-        with serial.Serial('COM8'):
+        with serial.Serial('COM8'): #put the name of port arduino is connected to.
             return True
     except serial.SerialException:
         return False
@@ -12,11 +12,12 @@ def is_com4_connected():
 while True:
     if is_com4_connected():
         if val==0:
-            print("COM8 connected! Opening Python program...")
-            subprocess.run(['python', 'driver.py'])
+            # print("COM8 connected! Opening Python program...")
+            subprocess.run(['python', 'joystick.py'])
             val=1
         elif val == 1 :
+            # print("connection established")
             continue
     else :
-        print("not connected")
+        # print("not connected")
         val=0 
